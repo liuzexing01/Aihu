63a8fa9a99c34069f15e9618caf44162a7e86238
liuzexing01 <lzxing180@163.com>
2018-10-28 21:14:45 +0800

    update
    update,lzx,2018/10/28

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const pic=require('./routes/pic.js');
 const cases=require('./routes/cases.js');
+const casesdetails=require('./routes/casesdetails.js');
 
 var app=express();
 app.listen(3000);
@@ -17,3 +18,4 @@ app.use(bodyParser.urlencoded({
 
 app.use('/pic',pic);
 app.use('/cases',cases);
+app.use('/casesdetails',casesdetails);
--- routes/cases.js
+++ routes/cases.js
@@ -3,7 +3,10 @@ const router=express.Router();
 const pool=require("../pool.js");
 
 router.get("/list",(req,res)=>{
-    
+    var sql="SELECT `cid`, `title`, `images`, `description`,`points`,`indexImg` FROM `cases`";
+    pool.query(sql,(err,result)=>{
+        if(err) throw err;
+        res.send(result)
     })
 })
 
